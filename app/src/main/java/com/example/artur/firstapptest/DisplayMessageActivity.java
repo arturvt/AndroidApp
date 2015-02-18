aa242759851f5f79d9f33be649c086e060f7dffd
Artur Tenorio <artur_tenorio@apple.com>
2015-02-18 18:31:35 -0300

    Initial commit

---
+++ app/src/main/java/com/example/artur/firstapptest/DisplayMessageActivity.java
@@ -0,0 +1,43 @@
+package com.example.artur.firstapptest;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class DisplayMessageActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        // Gets the message from HelloWorldActivity
+        String message = intent.getStringExtra(HelloWordActivity.EXTRA_MESSAGE);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        //Set the text view as the activity layout
+        setContentView(textView);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
